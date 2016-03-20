0f395fa945191c6783342fe9e35df5bd592c9361
andela-joyebanji <oyebanji.jacob@andela.com>
2016-03-21 00:19:26 +0100

    [Chore] Refactor App class
    Abstract Database Manager and Tables creation logic into methods
    Add setUpDatabaseManager method to set up Database Manager
    Add setUpDatabaseSchema method to set up database tables

--- src/App.php
+++ src/App.php
@@ -4,29 +4,57 @@ namespace Pyjac\NaijaEmoji;
 
 class App
 {
+    /**
+     * Stores an instance of the Slim application.
+     * @var \Slim\App
+     */
+    protected $app;
+
+    function __construct()
+    {
+        $settings = require __DIR__ . '/../src/settings.php';
+        $app = new \Slim\App($settings);
+
+        // Set up dependencies
+        require __DIR__ . '/../src/dependencies.php';
+
+        // Register routes
+        require __DIR__ . '/../src/routes.php';
+
+        $this->app = $app;
+        $this->setUpDatabaseManager();
+        $this->setUpDatabaseSchema();
+    }
+
+    /**
+     * Setup Eloquent ORM.
+     */
+    private function setUpDatabaseManager()
+    {
+        // Register the database connection with Eloquent
+        $capsule = $this->app->getContainer()->get('capsule');
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+    }
+
+    /**
+     * Create necessary database tables needed in the application.
+     */
+    private function setUpDatabaseSchema()
+    {
+        try {
+            DatabaseSchema::createTables();
+        } catch (\Exception $e) {
+            // This exception would be caught by the global exception handler.
+        }
+    }
+
+    /**
+     * Get an instance of the application.
+     * @return \Slim\App
+     */
     public  function get()
     {
-            $settings = require __DIR__ . '/../src/settings.php';
-            $app = new \Slim\App($settings);
-
-
-            // Set up dependencies
-            require __DIR__ . '/../src/dependencies.php';
-
-            // Register routes
-            require __DIR__ . '/../src/routes.php';
-
-            // Register the database connection with Eloquent
-            $capsule = $app->getContainer()->get('capsule');
-            $capsule->setAsGlobal();
-            $capsule->bootEloquent();
-
-            try {
-                DatabaseSchema::createTables();
-            } catch (Exception $e) {
-                // This exception would be caught by the global exception handler.
-            }
-         
-        return $app;
+        return $this->app;
     }
 }
