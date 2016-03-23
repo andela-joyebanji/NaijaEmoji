3d60c30cdc59815dc9f6bb7bffa08d8004d3959e
Oyebanji Jacob Mayowa <oyebanji.jacob@andela.com>
2016-03-23 04:45:14 -0400

    Applied fixes from StyleCI

--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -6,39 +6,38 @@ use Firebase\JWT\JWT;
 
 class AuthMiddleware
 {
-
     /**
-     * middleware invokable class
+     * middleware invokable class.
      *
-     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
-     * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
-     * @param  callable                                 $next     Next middleware
+     * @param \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+     * @param \Psr\Http\Message\ResponseInterface      $response PSR7 response
+     * @param callable                                 $next     Next middleware
      *
      * @return \Psr\Http\Message\ResponseInterface
      */
     public function __invoke($request, $response, $next)
     {
         if (!$request->hasHeader('authorization')) {
-            throw new \UnexpectedValueException("Token not provided");
+            throw new \UnexpectedValueException('Token not provided');
         }
         // Get the authorization header value in other to retrieve the token
         $authHeader = $request->getHeader('authorization');
         list($userJwt) = sscanf($authHeader[0], 'Bearer %s');
-        if(!$userJwt){
-            throw new \UnexpectedValueException("Token not provided");
+        if (!$userJwt) {
+            throw new \UnexpectedValueException('Token not provided');
         }
         $appSecret = getenv('APP_SECRET');
-        $jwtAlgorithm = getenv('JWT_ALGORITHM'); 
-        $jwtToken = JWT::decode($userJwt, $appSecret,[$jwtAlgorithm]);
-        
-        $user = \Pyjac\NaijaEmoji\Model\User::with("blacklistedTokens")->where('id', $jwtToken->data->userId)->first();
-        
-        if($user->blacklistedTokens()->where("token_jti",$jwtToken->jti)->get()->first()){
+        $jwtAlgorithm = getenv('JWT_ALGORITHM');
+        $jwtToken = JWT::decode($userJwt, $appSecret, [$jwtAlgorithm]);
+
+        $user = \Pyjac\NaijaEmoji\Model\User::with('blacklistedTokens')->where('id', $jwtToken->data->userId)->first();
+
+        if ($user->blacklistedTokens()->where('token_jti', $jwtToken->jti)->get()->first()) {
             throw new \DomainException();
         }
         $request = $request->withAttribute('user', $user);
         $request = $request->withAttribute('token_jti', $jwtToken->jti);
-        return $next($request, $response);
 
+        return $next($request, $response);
     }
 }
