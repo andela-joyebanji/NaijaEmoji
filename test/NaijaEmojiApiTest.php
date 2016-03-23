855d701d9f75628375418d04b8b813e0c6df941d
andela-joyebanji <oyebanji.jacob@andela.com>
2016-03-23 11:36:54 +0100

    [Chore] Refactor test file
    Add get,post, and postWithToken helper methods to handle GET and POST request to the application under test
    Ensure all test cases use the new helper methods

--- test/NaijaEmojiApiTest.php
+++ test/NaijaEmojiApiTest.php
@@ -13,24 +13,55 @@ class NaijaEmojiApiTest extends PHPUnit_Framework_TestCase
 {
     protected $app;
     protected $user;
+    protected $registerErrorMessage;
 
     public function setUp()
     {
         $this->app = (new App())->get();
         $this->user = TestDatabasePopulator::populate();
+        $this->registerErrorMessage = 'Username or Password field not provided.'; 
+    }
+
+    private function get($url)
+    {
+        $env = Environment::mock([
+            'REQUEST_METHOD' => 'GET',
+            'REQUEST_URI'    => $url,
+            ]);
+        $req = Request::createFromEnvironment($env);
+        $this->app->getContainer()['request'] = $req;
+
+        return $this->app->run(true);    
+    }
+
+    private function post($url,$body)
+    {
+        $env = Environment::mock(array(
+            'REQUEST_METHOD' => 'POST',
+            'REQUEST_URI'    => $url,
+            'CONTENT_TYPE'   => 'application/x-www-form-urlencoded'
+        ));
+        $req = Request::createFromEnvironment($env)->withParsedBody($body);
+        $this->app->getContainer()['request'] = $req;
+        return $this->app->run(true);  
+    }
+
+    private function postWithToken($url,$token,$body)
+    {
+        $env = Environment::mock(array(
+            'REQUEST_METHOD'     => 'POST',
+            'REQUEST_URI'        => $url,
+            'HTTP_AUTHORIZATION' => "Bearer ".$token,
+            'CONTENT_TYPE'       => 'application/x-www-form-urlencoded'
+        ));
+        $req = Request::createFromEnvironment($env)->withParsedBody($body);
+        $this->app->getContainer()['request'] = $req;
+        return $this->app->run(true);  
     }
 
     public function testLoginReturnsTokenWhenValidUsernameAndPasswordIsPassed()
     {
-        $env = Environment::mock(array(
-            'REQUEST_METHOD' => 'POST',
-            'REQUEST_URI' => '/auth/login',
-            'CONTENT_TYPE' => 'application/x-www-form-urlencoded'
-        ));
-        $req = Request::createFromEnvironment($env);
-        $req = $req->withParsedBody(['username' => 'tester','password' => 'test']);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
+        $response = $this->post('/auth/login',['username' => 'tester','password' => 'test']);
         $result = json_decode($response->getBody(), true);
         $this->assertNotNull($result['token']);
         $this->assertSame($response->getStatusCode(), 200);
@@ -38,15 +69,7 @@ class NaijaEmojiApiTest extends PHPUnit_Framework_TestCase
 
     public function testLoginReturnsStatusCode401WhenCorrectUsernameWithWrongPasswordIsPassed()
     {
-        $env = Environment::mock(array(
-            'REQUEST_METHOD' => 'POST',
-            'REQUEST_URI' => '/auth/login',
-            'CONTENT_TYPE' => 'application/x-www-form-urlencoded'
-        ));
-        $req = Request::createFromEnvironment($env);
-        $req = $req->withParsedBody(['username' => 'tester','password' => 'tes']);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
+        $response = $this->post('/auth/login',['username' => 'tester','password' => 'tes']);
         $result = json_decode($response->getBody(), true);
         $this->assertFalse(isset($result['token']));
         $this->assertSame($response->getStatusCode(), 401);
@@ -54,15 +77,7 @@ class NaijaEmojiApiTest extends PHPUnit_Framework_TestCase
 
     public function testLoginReturnsStatusCode401WhenIncorrectUsernameWithPasswordIsPassed()
     {
-        $env = Environment::mock(array(
-            'REQUEST_METHOD' => 'POST',
-            'REQUEST_URI' => '/auth/login',
-            'CONTENT_TYPE' => 'application/x-www-form-urlencoded'
-        ));
-        $req = Request::createFromEnvironment($env);
-        $req = $req->withParsedBody(['username' => '@tester','password' => 'tes']);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
+        $response = $this->post('/auth/login',['username' => '@tester','password' => 'tes']);
         $result = json_decode($response->getBody(), true);
         $this->assertFalse(isset($result['token']));
         $this->assertSame($response->getStatusCode(), 401);
@@ -71,16 +86,8 @@ class NaijaEmojiApiTest extends PHPUnit_Framework_TestCase
 
     public function testGetAllEmojisReturnsOneEmoji()
     {
-        $env = Environment::mock([
-            'REQUEST_METHOD' => 'GET',
-            'REQUEST_URI'    => '/emojis',
-            'PATH_INFO'      => '/emojis',
-            ]);
-        $req = Request::createFromEnvironment($env);
-        $this->app->getContainer()['request'] = $req;
-
-        $response = $this->app->run(true);
-
+    
+        $response = $this->get('/emojis');
         $data = json_decode($response->getBody(), true);
         $this->assertSame($response->getStatusCode(), 200);
         $this->assertSame($data[0]['name'], 'Suliat');
@@ -91,15 +98,7 @@ class NaijaEmojiApiTest extends PHPUnit_Framework_TestCase
     public function testGetEmojiReturnsCorrectEmojiWithStatusCodeOf200()
     {
         $emoji = $this->user->emojis()->first();
-        $env = Environment::mock([
-            'REQUEST_METHOD' => 'GET',
-            'REQUEST_URI'    => '/emojis/'.$emoji->id,
-            'PATH_INFO'      => '/emojis',
-            ]);
-        $req = Request::createFromEnvironment($env);
-        $this->app->getContainer()['request'] = $req;
-
-        $response = $this->app->run(true);
+        $response = $this->get('/emojis/'.$emoji->id);
         $data = json_decode($response->getBody(), true);
         $this->assertSame($response->getStatusCode(), 200);
         $this->assertSame($data['name'], $emoji->name);
@@ -108,16 +107,7 @@ class NaijaEmojiApiTest extends PHPUnit_Framework_TestCase
 
     public function testGetEmojiReturnsStatusCodeOf404WithMsgWhenEmojiWithPassedIdNotFound()
     {
-        $emoji = $this->user->emojis()->first();
-        $env = Environment::mock([
-            'REQUEST_METHOD' => 'GET',
-            'REQUEST_URI'    => '/emojis/10000',
-            'PATH_INFO'      => '/emojis',
-            ]);
-        $req = Request::createFromEnvironment($env);
-        $this->app->getContainer()['request'] = $req;
-
-        $response = $this->app->run(true);
+        $response = $this->get('/emojis/as3#');
         $data = json_decode($response->getBody(), true);
         $this->assertSame($response->getStatusCode(), 404);
         $this->assertSame($data['message'], 'The requested Emoji is not found.');
@@ -126,15 +116,7 @@ class NaijaEmojiApiTest extends PHPUnit_Framework_TestCase
 
     public function testRegisterReturnsStatusCode201WithMsgWhenUniqueUsernameAndPasswordIsPassed()
     {
-        $env = Environment::mock(array(
-            'REQUEST_METHOD' => 'POST',
-            'REQUEST_URI' => '/auth/register',
-            'CONTENT_TYPE' => 'application/x-www-form-urlencoded'
-        ));
-        $req = Request::createFromEnvironment($env);
-        $req = $req->withParsedBody(['username' => 'pyjac','password' => 'pyjac']);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
+        $response = $this->post('/auth/register',['username' => 'pyjac','password' => 'pyjac']);
         $result = json_decode($response->getBody(), true);
         $this->assertEquals($result['message'],'User successfully created.');
         $this->assertSame($response->getStatusCode(), 201);
@@ -142,104 +124,65 @@ class NaijaEmojiApiTest extends PHPUnit_Framework_TestCase
 
     public function testRegisterReturnsStatusCode409WithMsgWhenAlreadyExistingUsernameWithPasswordIsPassed()
     {
-        $env = Environment::mock(array(
-            'REQUEST_METHOD' => 'POST',
-            'REQUEST_URI' => '/auth/register',
-            'CONTENT_TYPE' => 'application/x-www-form-urlencoded'
-        ));
-        $req = Request::createFromEnvironment($env);
-        $req = $req->withParsedBody(['username' => 'tester','password' => 'pyjac']);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
+
+        $response = $this->post('/auth/register',['username' => 'tester','password' => 'pyjac']);
         $result = json_decode($response->getBody(), true);
         $this->assertEquals($result['message'],'Username already exist.');
         $this->assertSame($response->getStatusCode(), 409);
     }
 
-
-    public function testRegisterReturnsStatusCode400WithMsgWhenUsernameOrPasswordIsNotPassed()
+     public function testRegisterReturnsStatusCode400WithMsgWhenOnlyUsernameIsPassed()
     {
-        $env = Environment::mock(array(
-            'REQUEST_METHOD' => 'POST',
-            'REQUEST_URI' => '/auth/register',
-            'CONTENT_TYPE' => 'application/x-www-form-urlencoded'
-        ));
-        $authError = 'Username or Password field not provided.';
-        $req = Request::createFromEnvironment($env);
-        //When username or password is not passed
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
+        $response = $this->post('/auth/register', ['username' => 'tester']);
         $result = json_decode($response->getBody(), true);
-        $this->assertEquals($result['message'], $authError);
+        $this->assertEquals($result['message'], $this->registerErrorMessage);
         $this->assertSame($response->getStatusCode(), 400);
-
-        //When only username passed
-        $req = Request::createFromEnvironment($env);
-        $req = $req->withParsedBody(['username' => 'tester']);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
-        $result = json_decode($response->getBody(), true);
-        $this->assertEquals($result['message'], $authError);
-        $this->assertSame($response->getStatusCode(), 400);
-
-        //When only password passed
-        $req = $req->withParsedBody(['password' => 'tester']);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
-        $result = json_decode($response->getBody(), true);
-        $this->assertEquals($result['message'], $authError);
-        $this->assertSame($response->getStatusCode(), 400);  
     }
 
-     public function testRegisterReturnsStatusCode400WithMsgWhenUsernameOrPasswordIsPassedButEmpty()
+    public function testRegisterReturnsStatusCode400WithMsgWhenOnlyPasswordIsPassed()
     {
-        $env = Environment::mock(array(
-            'REQUEST_METHOD' => 'POST',
-            'REQUEST_URI' => '/auth/register',
-            'CONTENT_TYPE' => 'application/x-www-form-urlencoded'
-        ));
-        $authError = 'Username or Password field not provided.';
-        $req = Request::createFromEnvironment($env);
-
-        //When only username is passed with empty space
-        $req = Request::createFromEnvironment($env);
-        $req = $req->withParsedBody(['username' => ' ']);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
+        $response = $this->post('/auth/register', ['password' => 'tester']);
         $result = json_decode($response->getBody(), true);
-        $this->assertEquals($result['message'], $authError);
+        $this->assertEquals($result['message'], $this->registerErrorMessage);
+        $this->assertSame($response->getStatusCode(), 400);
+    }
+
+    public function testRegisterReturnsStatusCode400WithMsgWhenUsernameAndPasswordIsNotPassed()
+    {
+        $response = $this->post('/auth/register', []);
+        $result = json_decode($response->getBody(), true);
+        $this->assertEquals($result['message'], $this->registerErrorMessage);
         $this->assertSame($response->getStatusCode(), 400);
 
-        //When only password is passed with empty space
-        $req = Request::createFromEnvironment($env);
-        $req = $req->withParsedBody(['password' => ' ']);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
-        $result = json_decode($response->getBody(), true);
-        $this->assertEquals($result['message'], $authError);
-        $this->assertSame($response->getStatusCode(), 400);
+    }
 
-         //When both username and password are passed with empty space
-        $req = Request::createFromEnvironment($env);
-        $req = $req->withParsedBody(['username' => ' ','password' => ' ']);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
+    public function testRegisterReturnsStatusCode400WithMsgWhenOnlyUsernameIsPassedWithEmptyString()
+    {
+        $response = $this->post('/auth/register', ['username' => ' ']);
         $result = json_decode($response->getBody(), true);
-        $this->assertEquals($result['message'], $authError);
+        $this->assertEquals($result['message'], $this->registerErrorMessage);
         $this->assertSame($response->getStatusCode(), 400);
+    }
 
+    public function testRegisterReturnsStatusCode400WithMsgWhenOnlyPasswordIsPassedWithEmptyString()
+    {
+        $response = $this->post('/auth/register', ['password' => ' ']);
+        $result = json_decode($response->getBody(), true);
+        $this->assertEquals($result['message'], $this->registerErrorMessage);
+        $this->assertSame($response->getStatusCode(), 400);
+    }
+
+    public function testRegisterReturnsStatusCode400WithMsgWhenUsernameAndPasswordIsPassedWithEmptyStrings()
+    {
+        $response = $this->post('/auth/register', ['username' => ' ','password' => ' ']);
+        $result = json_decode($response->getBody(), true);
+        $this->assertEquals($result['message'], $this->registerErrorMessage);
+        $this->assertSame($response->getStatusCode(), 400);
     }
 
     public function testLogoutReturnsStatusCode400WithMsgWhenAuthorizationHeaderIsNotSet()
     {
-        $env = Environment::mock(array(
-            'REQUEST_METHOD' => 'POST',
-            'REQUEST_URI' => '/auth/logout',
-            'CONTENT_TYPE' => 'application/x-www-form-urlencoded'
-        ));
-        $req = Request::createFromEnvironment($env);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
+        $response = $this->post('/auth/logout',[]);
         $result = json_decode($response->getBody(), true);
         $this->assertEquals($result['message'],'Token not provided');
         $this->assertSame($response->getStatusCode(), 400);
@@ -263,15 +206,7 @@ class NaijaEmojiApiTest extends PHPUnit_Framework_TestCase
 
     public function testLogoutReturnsStatusCode400WithMsgWhenInvalidTokenIsPassed()
     {
-        $env = Environment::mock(array(
-            'REQUEST_METHOD' => 'POST',
-            'REQUEST_URI' => '/auth/logout',
-            'HTTP_AUTHORIZATION' => "Bearer lblkbbbvvgjh",
-            'CONTENT_TYPE' => 'application/x-www-form-urlencoded'
-        ));
-        $req = Request::createFromEnvironment($env);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
+        $response = $this->postWithToken('/auth/logout',"lblkbbbvvgjh", []);
         $result = json_decode($response->getBody(), true);
         $this->assertEquals($result['message'],'Wrong number of segments');
         $this->assertSame($response->getStatusCode(), 400);
@@ -279,18 +214,8 @@ class NaijaEmojiApiTest extends PHPUnit_Framework_TestCase
 
      public function testLogoutReturnsStatusCode200WithMsgWhenValidTokenIsPassed()
     {
-
         $token = $this->getLoginTokenForTestUser();
-        $token = "Bearer ".$token;
-        $env = Environment::mock([
-            'REQUEST_METHOD'     => 'POST',
-            'REQUEST_URI'        => '/auth/logout',
-            'HTTP_AUTHORIZATION' => $token,
-            'CONTENT_TYPE'       => 'application/x-www-form-urlencoded'
-        ]);
-        $reqs = Request::createFromEnvironment($env);
-        $this->app->getContainer()['request'] = $reqs;
-        $response = $this->app->run(true);
+        $response = $this->postWithToken('/auth/logout',$token, []);
         $result = (string)$response->getBody();
         $this->assertContains('Logout Successful',$result);
         $this->assertSame($response->getStatusCode(), 200);
@@ -298,15 +223,7 @@ class NaijaEmojiApiTest extends PHPUnit_Framework_TestCase
 
     private function getLoginTokenForTestUser()
     {
-        $env = Environment::mock(array(
-            'REQUEST_METHOD' => 'POST',
-            'REQUEST_URI' => '/auth/login',
-            'CONTENT_TYPE' => 'application/x-www-form-urlencoded'
-        ));
-        $req = Request::createFromEnvironment($env);
-        $req = $req->withParsedBody(['username' => 'tester','password' => 'test']);
-        $this->app->getContainer()['request'] = $req;
-        $response = $this->app->run(true);
+        $response = $this->post('/auth/login',['username' => 'tester','password' => 'test']);
         $result = json_decode($response->getBody(), true);
 
         return $result['token'];
